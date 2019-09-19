309db5d4170ed9884b7b0d624545af30981213dc
wu-jeffrey <j387wu@edu.uwaterloo.ca>
2019-09-18 21:57:11 -0400

    using React/firebase WSL not cooperating w/ rails >:(

---
+++ src/components/landing-page/landing-page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './landing-page.css';
+
+function LandingPage() {
+  return (
+    <React.Fragment>
+      <div>
+
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default LandingPage;
